254375786769ac66efc1829ff7f999108556f0e4
Alexey Selivanov <lazypacman@gmail.com>
2024-02-26 15:17:29 +0400

    First commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,113 +1,99 @@
-import Image from "next/image";
+'use client';
+
+import { ChangeEvent, useState } from 'react';
 
 export default function Home() {
+  const [dec, setDec] = useState<string>('0');
+  const [hex, setHex] = useState<string>('0');
+  const [bin, setBin] = useState<string>('0');
+  const [oct, setOct] = useState<string>('0');
+
+  function handleDecChange({ target: { value } }: ChangeEvent<HTMLInputElement>) {
+    let parsed = parseInt(value);
+
+    if (isNaN(parsed)) parsed = 0;
+
+    setBin(parsed.toString(2));
+    setOct(parsed.toString(8));
+    setDec(value);
+    setHex(parsed.toString(16));
+  }
+
+  function handleBinChange({ target: { value } }: ChangeEvent<HTMLInputElement>) {
+    let parsed = parseInt(value, 2);
+
+    if (isNaN(parsed)) parsed = 0;
+
+    setBin(value);
+    setOct(parsed.toString(8));
+    setDec(parsed.toString(10));
+    setHex(parsed.toString(16));
+  }
+
+  function handleOctChange({ target: { value } }: ChangeEvent<HTMLInputElement>) {
+    let parsed = parseInt(value, 8);
+
+    if (isNaN(parsed)) parsed = 0;
+
+    setBin(parsed.toString(2));
+    setOct(value);
+    setDec(parsed.toString(10));
+    setHex(parsed.toString(16));
+  }
+
+  function handleHexChange({ target: { value } }: ChangeEvent<HTMLInputElement>) {
+    let parsed = parseInt(value, 16);
+
+    if (isNaN(parsed)) parsed = 0;
+
+    setBin(parsed.toString(2));
+    setOct(parsed.toString(8));
+    setDec(parsed.toString(10));
+    setHex(value);
+  }
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
+    <main className="mt-48 h-screen flex flex-col gap-4 font-mono  items-center text-2xl">
+      <label className="flex gap-2 items-center">
+        <div className="uppercase  text-slate-700">Dec</div>
+        <input
+          className="border-2 rounded px-2 invalid:bg-red-200"
+          type="text"
+          value={dec}
+          onChange={handleDecChange}
+          pattern="[0-9]+"
         />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
+      </label>
+      <label className="flex gap-2 items-center">
+        <div className="uppercase  text-slate-400">Bin</div>
+        <input
+          className="border-2 rounded px-2 invalid:bg-red-200"
+          type="text"
+          value={bin}
+          onChange={handleBinChange}
+          pattern="[0-1]+"
+        />
+      </label>
+      <label className="flex gap-2 items-center">
+        <div className="uppercase  text-slate-400">Oct</div>
+        <input
+          className="border-2 rounded px-2 invalid:bg-red-200"
+          type="text"
+          value={oct}
+          pattern="[0-7]+"
+          onChange={handleOctChange}
+        />
+      </label>
+      <label className="flex gap-2 items-center">
+        <div className="uppercase  text-slate-400">Hex</div>
+        <input
+          className="border-2 rounded px-2 invalid:bg-red-200"
+          type="text"
+          value={hex}
+          onChange={handleHexChange}
+          pattern="[A-Fa-f0-9]+"
+        />
+      </label>
     </main>
   );
 }
